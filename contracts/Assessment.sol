d226b6a02e4b577d794fa31f7496889f71f6f7c8
Ajay702 <74392659+Ajay702@users.noreply.github.com>
2024-07-11 01:39:14 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,18 +1,15 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 contract Assessment {
-    // Function to sum two integers
     function sum(int a, int b) public pure returns (int) {
         return a + b;
     }
 
-    // Function to check if a number is even
     function isEven(int num) public pure returns (bool) {
         return num % 2 == 0;
     }
 
-    // Function to return the length of a string
     function lengthString(string memory str) public pure returns (uint) {
         return bytes(str).length;
     }
